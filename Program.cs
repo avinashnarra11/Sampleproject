2111e5430131f8529bd08f2dbad1c894b6d477b2
tokalanandakishore <tokalanandakishore@gmail.com>
2019-08-26 15:20:13 -0400

    decommented

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Sampleproject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //Console.ReadKey();
+            Console.ReadKey();
 
         }
     }
